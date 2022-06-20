050854acc069ddf43f67fb91f523d04a0a462c00
vdkjg <hweichelt@uni-potsdam.de>
2022-06-20 13:29:54 +0200

    added iterative deletion muc algorithm plus restructure

--- sudoku_muc/main.py
+++ sudoku_muc/main.py
@@ -1,42 +1,73 @@
+import os
+import time
+
 import clingo
-from minimal_unsatisfiable_core import Util, MUCSimple
+from minimal_unsatisfiable_core import Util, MUC
+
+from clingraph.orm import Factbase
+from clingraph.graphviz import compute_graphs, render
+
+
+def muc_sudoku():
+    assumption_list = [
+        # adding assumptions
+        # CORRECT ASSUMPTIONS
+        # TODO : is the (_, True) necessary?
+        (clingo.Function("guess", [clingo.Number(4), clingo.Number(9), clingo.Number(3)]), True),
+        (clingo.Function("guess", [clingo.Number(7), clingo.Number(1), clingo.Number(9)]), True),
+        (clingo.Function("guess", [clingo.Number(2), clingo.Number(2), clingo.Number(7)]), True),
+        (clingo.Function("guess", [clingo.Number(5), clingo.Number(7), clingo.Number(7)]), True),
+        (clingo.Function("guess", [clingo.Number(3), clingo.Number(9), clingo.Number(7)]), True),
+        (clingo.Function("guess", [clingo.Number(8), clingo.Number(2), clingo.Number(8)]), True),
+        (clingo.Function("guess", [clingo.Number(1), clingo.Number(6), clingo.Number(8)]), True),
+        (clingo.Function("guess", [clingo.Number(6), clingo.Number(7), clingo.Number(8)]), True),
+        (clingo.Function("guess", [clingo.Number(2), clingo.Number(9), clingo.Number(8)]), True),
+        # CONFLICTING ASSUMPTIONS
+        # double value for cell
+        # (clingo.Function("guess", [clingo.Number(4), clingo.Number(9), clingo.Number(5)]), True),
+        # value at the wrong position in cage (double 7 in cage(1,1))
+        # (clingo.Function("guess", [clingo.Number(2), clingo.Number(3), clingo.Number(7)]), True),
+    ]
+
+    program = "res/sudoku_only_rules.lp"
+    instance = "res/instances/sudoku_instance_1.lp"
+    visualization = "res/visualization/visualize_sudoku.lp"
+
+    print("T0 : SOLVING START")
+    # TODO : cannot use the solve handle outside the function. Is this bc of async/parallel programming?
+    (satisfiable, model_string, core) = Util.solve(
+        program=program,
+        instance=instance,
+        assumption_list=assumption_list
+    )
+
+    print("result : ", ["UNSAT", "SAT"][satisfiable])
+    print("model : ", model_string)
+    print("core : ", core)
+
+    if satisfiable:
+        Util.render_sudoku(model_string, visualization_file=visualization)
+
+    print("T0 : SOLVING END")
+    print("T1 : FINDING MUC START")
+
+    unsatisfiable, muc = MUC.muc_iterative_deletion(
+        program=program,
+        instance=instance,
+        assumption_list=assumption_list
+    )
+
+    if not unsatisfiable:
+        print("MUC : Problem wasn't unsatisfiable to begin with, there is no minimal unsatisfiable core")
+    else:
+        print(f"MUC : {muc}")
+
+    print("T1 : FINDING MUC END")
+
+    # TODO : what do the ints in the uc mean ???
 
 
 if __name__ == '__main__':
-    print('SUDOKU - MINIMAL UNSATISFIABLE CORE TEST')
-
-    ctl = clingo.Control()
-
-    # program_string = Util.get_file_content_str('res/sudoku_solver.lp')
-
-    program_string = Util.get_file_content_str('res/sudoku_only_rules.lp')
-    instance_string = Util.get_file_content_str('res/instances/sudoku_instance_1.lp')
-    visualization_string = Util.get_file_content_str('res/visualization/visualize_sudoku.lp')
-
-    muc = MUCSimple()
-
-    ctl.add("base", [], f"{program_string}\n{instance_string}\n{visualization_string}")
-    ctl.ground([("base", [])])
-    assumption_list = [
-        # adding the assumption of : solution(2,2,7) [SAT] or solution(2,2,5) [UNSAT]
-        # (clingo.Function("solution", [clingo.Number(2), clingo.Number(2), clingo.Number(7)]), True),
-        # (clingo.Function("solution", [clingo.Number(7), clingo.Number(1), clingo.Number(9)]), True),
-        # (clingo.Function("solution", [clingo.Number(4), clingo.Number(9), clingo.Number(3)]), True),
-    ]
-    solve_handle = ctl.solve(assumptions=assumption_list, yield_=True)
-    print(solve_handle)
-    print("result : ", solve_handle.get())  # blocking until the result is ready
-    print("model : ", solve_handle.model())
-    print("core : ", solve_handle.core(), " = ", [assumption_list[i] for i in solve_handle.core()])
-
-    # TODO : get the clingraph visualization to work
-    # TODO : - this command shows still wrong numbers at the moment
-    # TODO : - command line command :
-    #  clingo instances/sudoku_instance_1.lp sudoku_solver.lp -n 0 --outf=2 |
-    #  clingraph --view --dir='out/sudoku' --format=png --out=render --prefix=viz_ --engine=neato --default-graph=sudoku
-    #  --viz-encoding=visualization/visualize_sudoku.lp --name-format=model_{model_number}
-
-    # TODO : implement the clingraph visualization directly into the script
-
-    # TODO : find why it returns UNSAT when I use any assumptions for not yet filled cells
+    # example_clingraph()
+    muc_sudoku()
 
--- sudoku_muc/minimal_unsatisfiable_core.py
+++ sudoku_muc/minimal_unsatisfiable_core.py
@@ -1,4 +1,9 @@
+import os
 from abc import ABC, abstractmethod
+
+import clingo
+from clingraph.orm import Factbase
+from clingraph.graphviz import compute_graphs, render
 
 
 class Util(ABC):
@@ -9,30 +14,73 @@ class Util(ABC):
             out = f.read()
         return out
 
+    @staticmethod
+    def solve(program, instance, assumption_list=None):
+        ctl = clingo.Control()
+
+        program_string = Util.get_file_content_str(program)
+        instance_string = Util.get_file_content_str(instance)
+
+        ctl.add("base", [], program_string)
+        ctl.add("base", [], instance_string)
+
+        ctl.ground([("base", [])])
+
+        assumptions = assumption_list if assumption_list is not None else []
+        solve_handle = ctl.solve(assumptions=assumptions, yield_=True)
+
+        satisfiable = solve_handle.get().satisfiable
+        model_string = str(solve_handle.model()) if solve_handle.model() is not None else ""
+        core = solve_handle.core()
+
+        return satisfiable, model_string, core
+
+    @staticmethod
+    def render_sudoku(model_string, visualization_file):
+        # TODO : Remove os.system call
+        print("GENERATING CLINGRAPH FACTBASE VIA OS.SYSTEM : REMOVE!")
+        # Write the found model to res/models/temp_model.lp
+        with open("res/models/temp_model.lp", "w") as f:
+            f.write(".\n".join(model_string.split()) + ".")
+        os.system(
+            f"clingo res/models/temp_model.lp -n 0 --outf=2 | clingraph --view --out=facts --prefix=viz_ "
+            f"--default-graph=sudoku --viz-encoding={visualization_file} > res/factbases/factbase.lp "
+        )
+        print("CLINGRAPH FACTBASE GENERATED")
+
+        fb = Factbase(prefix="viz_", default_graph="sudoku")
+        fb.add_fact_file("res/factbases/factbase.lp")
+
+        # TODO : Use the internal function from model somehow
+        # fb = Factbase.from_model(solve_handle.model(), prefix="viz_")
+        # print(type(fb), fb)
+
+        graphs = compute_graphs(fb)
+
+        render(graphs, format="png", view=True, engine="neato")
+
 
 class MUC(ABC):
 
     @staticmethod
-    def on_model(m):
-        print("GOT MODEL")
-        print(type(m), m)
+    def muc_iterative_deletion(program, instance, assumption_list=None):
+        # TODO : This algorithm is not using the core but the assumption list. Maye there is a way to use the core
+        original_problem_unsatisfiable = False
+        (satisfiable, model_string, core) = Util.solve(
+            program=program,
+            instance=instance,
+            assumption_list=assumption_list
+        )
 
-    @staticmethod
-    def on_core(c):
-        print("GOT UNSATISFIABLE CORE")
-        print(type(c), c)
-        if isinstance(c, list):
-            for assumption in c:
-                print(f"---{type(assumption)} : {assumption}")
+        if satisfiable:
+            return original_problem_unsatisfiable, None
+        else:
+            original_problem_unsatisfiable = True
+            minimal_core = []
+            for i, assumption in enumerate(assumption_list):
+                partial_assumption_list = [a for a in assumption_list if a != assumption]
+                (sat, _, _) = Util.solve(program=program, instance=instance, assumption_list=partial_assumption_list)
+                if sat:
+                    minimal_core.append(assumption)
 
-    @staticmethod
-    @abstractmethod
-    def get_muc(c):
-        pass
-
-
-class MUCSimple(MUC):
-
-    @staticmethod
-    def get_muc(c):
-        print(c)
+            return original_problem_unsatisfiable, minimal_core
